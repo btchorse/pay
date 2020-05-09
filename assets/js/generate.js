ee122b6cd6d38a6f09060e0ca77c96598723b357
Rastislav <contact@satoshiplus.net>
2020-05-09 12:12:28 +0200

    Fix variables

--- assets/js/generate.js
+++ assets/js/generate.js
@@ -6,44 +6,44 @@ const params = url.searchParams;
 
 switch(protocol) {
 	case 'bitcoin':
-		const name = 'bitcoin';
-		const currency = '₿';
-		const address = url.hostname;
-		const amount = parseFloat(params.get('amount'));
-		const label = decodeURI(params.get('label'));
-		const message = decodeURI(params.get('message'));
+		let name = 'bitcoin';
+		let currency = '₿';
+		let address = url.hostname;
+		let amount = parseFloat(params.get('amount'));
+		let label = decodeURI(params.get('label'));
+		let message = decodeURI(params.get('message'));
 		break;
 	case 'litecoin':
-		const name = 'litecoin';
-		const currency = 'Ł';
-		const address = url.hostname;
-		const amount = parseFloat(params.get('amount'));
-		const label = decodeURI(params.get('label'));
-		const message = decodeURI(params.get('message'));
+		let name = 'litecoin';
+		let currency = 'Ł';
+		let address = url.hostname;
+		let amount = parseFloat(params.get('amount'));
+		let label = decodeURI(params.get('label'));
+		let message = decodeURI(params.get('message'));
 		break;
 	case 'ethereum':
-		const name = 'ethereum';
-		const currency = 'Ξ';
-		const address = url.hostname;
-		const amount = parseFloat(params.get('value'));
-		const label = '';
-		const message = decodeURI(params.get('data'));
+		let name = 'ethereum';
+		let currency = 'Ξ';
+		let address = url.hostname;
+		let amount = parseFloat(params.get('value'));
+		let label = '';
+		let message = decodeURI(params.get('data'));
 		break;
 	case 'payto':
-		const path = url.pathname.split('/');
-		const pay = decodeURI(params.get('amount')).split(':');
-		const currency = pay[0];
-		const amount = parseFloat(pay[1]);
-		const label = decodeURI(params.get('label'));
-		const message = decodeURI(params.get('message'));
+		let path = url.pathname.split('/');
+		let pay = decodeURI(params.get('amount')).split(':');
+		let currency = pay[0];
+		let amount = parseFloat(pay[1]);
+		let label = decodeURI(params.get('label'));
+		let message = decodeURI(params.get('message'));
 		break;
 	default:
-		const name = '';
-		const currency = '';
-		const address = '';
-		const amount = 0;
-		const label = '';
-		const message = '';
+		let name = '';
+		let currency = '';
+		let address = '';
+		let amount = 0;
+		let label = '';
+		let message = '';
 		alert('No payment found!');
 		break;
 }
@@ -63,12 +63,12 @@ var qr = new QRious({
 	value: link
 });
 
-var qrimage = qr.toDataURL('image/jpeg');
-var meta1 = document.createElement('meta');
+let qrimage = qr.toDataURL('image/jpeg');
+let meta1 = document.createElement('meta');
 meta1.setAttribute('property', 'og:image');;
 meta1.setAttribute('content', qrimage);
 document.getElementsByTagName('head')[0].appendChild(meta1);
-var meta2 = document.createElement('meta');
+let meta2 = document.createElement('meta');
 meta2.setAttribute('property', 'twitter:image');
 meta2.setAttribute('content', qrimage);
 document.getElementsByTagName('head')[0].appendChild(meta2);
@@ -81,9 +81,9 @@ if (address != null)
 	document.getElementById('address').innerHTML = address.match(/.{1,4}/g).join(' ');
 	document.getElementById('inputAddress').value = address;
 
-var copyAddress = document.getElementById('address');
+let copyAddress = document.getElementById('address');
 copyAddress.addEventListener('click', function () {
-	var addressField = document.getElementById('inputAddress');
+	let addressField = document.getElementById('inputAddress');
 	addressField.select();
 	document.execCommand('copy');
 }, false);
